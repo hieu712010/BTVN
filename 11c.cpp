6f113d0b04aeaec94e2c1805766bb6b3fd4ed3a2
hieu712010 <144592760+hieu712010@users.noreply.github.com>
2023-11-12 18:05:49 +0700

    Create 11c.cpp

---
+++ 11c.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#define ll long long
+
+using namespace std;
+
+int main(){
+    ll n ;float s = 1; cin >> n;
+    for(ll i = 2; i <= n; i++)
+        s+=1.0/(i*i);
+    cout << s;
+    return 0;
+}
