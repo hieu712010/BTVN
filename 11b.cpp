1cfdb4e38ca9635d41d3d308907dc99dccf92f5c
hieu712010 <144592760+hieu712010@users.noreply.github.com>
2023-11-12 18:05:14 +0700

    Create 11b.cpp

---
+++ 11b.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#define ll long long
+
+using namespace std;
+
+int main(){
+    ll n; float s = 0; cin >> n;
+    for(ll i = 2; i <= 2*n; i+=2)
+        s+=1.0/i;
+    cout << s;
+    return 0;
+}
