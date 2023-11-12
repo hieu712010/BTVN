b6f6b0a8b261004c34078007071d1a69f52db53d
hieu712010 <144592760+hieu712010@users.noreply.github.com>
2023-11-12 22:08:12 +0700

    Update 16.cpp

--- 16.cpp
+++ 16.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
+#define ll long long
+using namespace std;
 
+ll UCLN(ll a, ll b){
+    if(b == 0)
+        return a;
+    return UCLN(b, a % b);
+}
+int main(){
+    ll m, n; cin >> m >> n;
+
+    if(m == 0 && n == 0)
+        return 0;
+    cout << UCLN(m, n) << "\n" << m * n / UCLN(m, n);
+    return 0;
+}
