9f4f2aa83fc6cbdc2e98645df11dbd98cdd9a02e
hieu712010 <144592760+hieu712010@users.noreply.github.com>
2023-11-12 21:59:13 +0700

    Update 11h.cpp

--- 11h.cpp
+++ 11h.cpp
@@ -5,6 +5,8 @@ using namespace std;
 
 int main(){
     ll n, s = 0, tem = 1; cin >> n;
+    if(n <= 0) 
+        return 0;
     for(ll i = 1; i <= n; i++){
         tem *= i;
         if(i % 2 != 0)
