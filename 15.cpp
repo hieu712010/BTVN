8304c5cc2660fa8bbf1cdfc64d9ce5c08b50141b
hieu712010 <144592760+hieu712010@users.noreply.github.com>
2023-11-12 21:57:09 +0700

    Update 15.cpp

--- 15.cpp
+++ 15.cpp
@@ -1,1 +1,20 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
 
+int main(){
+    long long n; cin >> n;
+    if(n <= 0) 
+        return 0;
+    string s;
+    while(n > 0){
+        if(n % 2 == 0)
+            s.append("0");
+        else
+            s.append("1");
+        n /= 2;
+    }
+    reverse(begin(s), end(s));
+    cout << s;
+    return 0;
+}
